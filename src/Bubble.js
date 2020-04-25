6e6a4c599be2f9a331328a268000debd01db68ee
Manvendra Singh <mjdeora@gmail.com>
2020-04-26 01:13:20 +0530

    [Fix] Bubble slightly right position issue WIP

--- src/Bubble.js
+++ src/Bubble.js
@@ -3,8 +3,10 @@ import React from "react";
 export default function Slider(props) {
     const { value, visible } = props;
 
+    const position = 10;
+
     return (
-        <div className="bubble" style={{ left: `${value}%`, display: visible ? '' : 'none' }}>
+        <div className="bubble" style={{ left: `calc(${value}% - ${position}px)`, display: visible ? null : 'none' }}>
             <span>{value}</span>
         </div>
     );
