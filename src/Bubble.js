5737c95f145695279ad6568d311c9aba10716730
Manvendra Singh <mjdeora@gmail.com>
2020-04-26 00:05:43 +0530

    [Style] Style updated

--- src/Bubble.js
+++ src/Bubble.js
@@ -4,7 +4,7 @@ export default function Slider(props) {
     const { value } = props;
 
     return (
-        <div className="bubble">
+        <div className="bubble" style={{ left: `${value}%` }}>
             {value}
         </div>
     );
