43a8cf0fea51ef48308967d60610fb052bf5e918
Manvendra Singh <mjdeora@gmail.com>
2020-04-25 23:10:45 +0530

    [Feature] Slider component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import Slider from "./Slider";
 
 import "./styles.css";
 
 export default function App() {
+    let [value, setValue] = useState(0);
+
+    function handleChange(e) {
+        console.log(e.target.value);
+        setValue(e.target.value);
+    }
+
     return (
         <div className="App">
-            Hello
+            <div className="slider-container">
+                <Slider
+                    min={0}
+                    max={100}
+                    step={1}
+                    value={value}
+                    onChange={handleChange}
+                />
+            </div>
         </div>
     );
 }
