9532bba21e60146a6ea65705ef33bf50c994c4d7
Manvendra Singh <mjdeora@gmail.com>
2020-04-27 01:07:15 +0530

    [Refactor] Slider updated

--- src/components/FormSlider/FormSlider.js
+++ src/components/FormSlider/FormSlider.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
+
 import Slider from "../Slider/Slider";
 import Bubble from "../Bubble/Bubble";
 
 import styles from './FormSlider.module.css'
 
 export default function FormSlider(props) {
-    const { initialValue, min, max, onChange, showLables, showBubble, showBubbleOnlyOnScroll, ...otherProps } = props;
+    const { initialValue, onChange, showBubble, showBubbleOnlyOnScroll, ...otherProps } = props;
 
     let [visible, setVisible] = useState(!showBubbleOnlyOnScroll);
     let [value, setValue] = useState(initialValue);
@@ -26,40 +27,29 @@ export default function FormSlider(props) {
     const bubbleStyle = {
         position: 'absolute',
         left: `calc(${value}% - 17px)`,
-        bottom: showLables ? '50px' : '35px'
+        bottom: '35px'
     }
 
     return (
-        <div className={styles['slider-container']}>
+        <div className={styles['form-slider']}>
             {showBubble
                 && visible
                 && <Bubble value={value} style={bubbleStyle} />}
             <Slider
                 {...otherProps}
-                min={min}
-                max={max}
+                initialValue={initialValue}
                 handleChange={handleChange}
                 onMouseUp={onMouseUp}
                 onMouseDown={onMouseDown}
                 onTouchEnd={onMouseUp}
                 onTouchStart={onMouseDown}
             />
-            {showLables &&
-                <div className={styles.labels}>
-                    <span>{min}</span>
-                    <span>{max}</span>
-                </div>}
         </div>
     );
 }
 
 FormSlider.defaultProps = {
     initialValue: 50,
-    min: 0,
-    max: 100,
-    step: 1,
-    disabled: false,
-    showLables: true,
     showBubble: true,
     showBubbleOnlyOnScroll: true
 }
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import Input from "../Input/Input";
 
 import styles from './Slider.module.css'
 
 export default function Slider(props) {
-    const { initialValue, handleChange, className, ...otherProps } = props;
+    const { initialValue, handleChange, className, min, max, showLables, ...otherProps } = props;
 
     let [value, setValue] = useState(initialValue);
 
@@ -14,12 +14,32 @@ export default function Slider(props) {
     }
 
     return (
-        <Input
-            {...otherProps}
-            type="range"
-            value={value}
-            onChange={onChange}
-            className={`${styles.slider} ${className}`}
-        />
+        <div className={styles['slider-container']}>
+            <Input
+                {...otherProps}
+                min={min}
+                max={max}
+                type="range"
+                value={value}
+                onChange={onChange}
+                className={`${styles.slider} ${className}`}
+            />
+            {showLables &&
+                <Fragment>
+                    <span className={`${styles.label} ${styles.min}`}>{min}</span>
+                    <span className={`${styles.label} ${styles.max}`}>{max}</span>
+                </Fragment>}
+        </div>
     );
 }
+
+
+Slider.defaultProps = {
+    initialValue: 50,
+    min: 0,
+    max: 100,
+    step: 1,
+    disabled: false,
+    showLables: true,
+}
+
