34634d20b78304ae4b96e6eaeafb2e0b99ce814f
Manvendra Singh <mjdeora@gmail.com>
2020-04-27 00:00:24 +0530

    [Storybook] Base setup

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+const custom = require('../webpack.config.js');
+
+module.exports = {
+  stories: ['../src/**/*.stories.js'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  webpackFinal: async config => {
+    // do mutation to the config
+
+    return { ...config, module: { ...config.module, rules: custom.module.rules } };
+  },
+};
---
+++ src/components/FormSlider/FormSlider.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import FormSlider from './FormSlider'
+
+export default {
+    title: 'FormSlider',
+    component: FormSlider,
+};
+
+export const Simple = () => <FormSlider />;
